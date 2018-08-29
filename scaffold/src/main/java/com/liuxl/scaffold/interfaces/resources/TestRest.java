4a511ff12c64dee73672ecc22337d06b5d7acba9
liuxl <18720267623@163.com>
2018-08-29 15:15:09 +0800

    最开始的搭建

---
+++ scaffold/src/main/java/com/liuxl/scaffold/interfaces/resources/TestRest.java
@@ -0,0 +1,28 @@
+package com.liuxl.scaffold.interfaces.resources;
+
+import com.sun.jersey.spi.resource.Singleton;
+import org.springframework.stereotype.Controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:demo
+ *
+ * @author liuxl
+ * @date 2018/8/29
+ */
+@Controller()
+@Singleton
+@Path("/demo")
+public class TestRest {
+
+    @GET
+    @Path("/demo")
+    @Produces("application/json;charset=utf-8")
+    public String demo() {
+        return "demo";
+    }
+}
